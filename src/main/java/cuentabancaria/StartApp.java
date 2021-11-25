57572df1c09bf82e1a9881a8dc8095bc7cec7710
Johan Ramirez <johan911019@gmail.com>
2021-11-25 15:21:34 -0500

    Casos de prueba verificados

--- src/main/java/cuentabancaria/StartApp.java
+++ src/main/java/cuentabancaria/StartApp.java
@@ -4,8 +4,8 @@ public class StartApp {
     public static void main(String[] args) {
         Account account = new Account();
         account.deposit(1000, "10/01/2021");
-        account.withdraw(500, "13/01/2021");
-        account.withdraw(200, "16/01/2021");
+        account.deposit(2000, "13/01/2021");
+        account.withdraw(500, "14/01/2021");
         account.transfer(100, 1234567890, "18/01/2021");
         account.printStatements();
     }
