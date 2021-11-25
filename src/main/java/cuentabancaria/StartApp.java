e01fbc197c1730f3fbd6e548d9d666deec2bbd2f
Johan Ramirez <johan911019@gmail.com>
2021-11-25 15:18:06 -0500

    Solución se errores y revisión final

--- src/main/java/cuentabancaria/StartApp.java
+++ src/main/java/cuentabancaria/StartApp.java
@@ -5,7 +5,7 @@ public class StartApp {
         Account account = new Account();
         account.deposit(1000, "10/01/2021");
         account.withdraw(500, "13/01/2021");
-        account.withdraw(200, "16/02/2021");
+        account.withdraw(200, "16/01/2021");
         account.transfer(100, 1234567890, "18/01/2021");
         account.printStatements();
     }
